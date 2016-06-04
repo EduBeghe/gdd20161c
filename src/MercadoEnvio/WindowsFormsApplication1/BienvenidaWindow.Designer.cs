d45ab372ec881e84c1e79d5df6b2e3c97f0890a3
Mujics <Mujics03@gmail.com>
2016-06-04 09:15:15 -0300

    Estructura del Proyecto y DBAdapter

---
+++ src/MercadoEnvio/WindowsFormsApplication1/BienvenidaWindow.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MercadoEnvio
+{
+    partial class BienvenidaWindow
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 29);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(739, 64);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Bienvenido a Mercado Envio";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 110);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(421, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Invitado";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // BienvenidaWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(778, 244);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Name = "BienvenidaWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
