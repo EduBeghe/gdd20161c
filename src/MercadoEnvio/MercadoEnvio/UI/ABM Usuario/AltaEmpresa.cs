08173016114f3811b6c89db4f6bbff46969961af
Eduardo <eduardo@giro54.com>
2016-06-09 23:32:33 -0300

    Creo el FrontEnd de la parte de ABM de Usuarios

---
+++ src/MercadoEnvio/MercadoEnvio/UI/ABM Usuario/AltaEmpresa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MercadoEnvio.UI.ABM_Usuario
+{
+    public partial class AltaEmpresa : Form
+    {
+        public AltaEmpresa()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- src/MercadoEnvio/MercadoEnvio/UI/ABM Usuario/AltaUsuario.cs
+++ src/MercadoEnvio/MercadoEnvio/UI/ABM Usuario/AltaUsuario.cs
@@ -16,5 +16,10 @@ namespace MercadoEnvio.ABM_Usuario
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
