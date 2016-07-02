6147718208e7d81406ce26622ee9228ee17ef919
Eduardo <eduardo@giro54.com>
2016-07-02 19:38:59 -0300

    cambios

--- src/MercadoEnvio/MercadoEnvio/UI/ComprarOfertar/ComprarPublicacion.Designer.cs
+++ src/MercadoEnvio/MercadoEnvio/UI/ComprarOfertar/ComprarPublicacion.Designer.cs
@@ -38,51 +38,58 @@
             // 
             // cantidadTextBox
             // 
-            this.cantidadTextBox.Location = new System.Drawing.Point(175, 57);
+            this.cantidadTextBox.Location = new System.Drawing.Point(117, 37);
+            this.cantidadTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cantidadTextBox.Name = "cantidadTextBox";
-            this.cantidadTextBox.Size = new System.Drawing.Size(216, 26);
+            this.cantidadTextBox.Size = new System.Drawing.Size(145, 20);
             this.cantidadTextBox.TabIndex = 0;
             // 
             // FormaDePagoComboBox
             // 
             this.FormaDePagoComboBox.FormattingEnabled = true;
-            this.FormaDePagoComboBox.Location = new System.Drawing.Point(175, 104);
+            this.FormaDePagoComboBox.Location = new System.Drawing.Point(117, 68);
+            this.FormaDePagoComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.FormaDePagoComboBox.Name = "FormaDePagoComboBox";
-            this.FormaDePagoComboBox.Size = new System.Drawing.Size(216, 28);
+            this.FormaDePagoComboBox.Size = new System.Drawing.Size(145, 21);
             this.FormaDePagoComboBox.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(131, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.Size = new System.Drawing.Size(152, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Comprar Publicacion";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 63);
+            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Cantidad";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 112);
+            this.label3.Location = new System.Drawing.Point(15, 73);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.Size = new System.Drawing.Size(79, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Forma de Pago";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 158);
+            this.button1.Location = new System.Drawing.Point(63, 103);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(365, 32);
+            this.button1.Size = new System.Drawing.Size(149, 28);
             this.button1.TabIndex = 5;
             this.button1.Text = "Confirmar Compra";
             this.button1.UseVisualStyleBackColor = true;
@@ -90,15 +97,16 @@
             // 
             // ComprarPublicacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(420, 202);
+            this.ClientSize = new System.Drawing.Size(280, 142);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.FormaDePagoComboBox);
             this.Controls.Add(this.cantidadTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ComprarPublicacion";
             this.Text = "ComprarPublicacion";
             this.ResumeLayout(false);
--- src/MercadoEnvio/MercadoEnvio/UI/ComprarOfertar/OfertarPublicacion.Designer.cs
+++ src/MercadoEnvio/MercadoEnvio/UI/ComprarOfertar/OfertarPublicacion.Designer.cs
@@ -37,26 +37,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(111, 22);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.Size = new System.Drawing.Size(140, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Ofertar Publicacion";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 69);
+            this.label2.Location = new System.Drawing.Point(13, 45);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Oferta";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 108);
+            this.button1.Location = new System.Drawing.Point(71, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(334, 33);
+            this.button1.Size = new System.Drawing.Size(100, 26);
             this.button1.TabIndex = 2;
             this.button1.Text = "Confirmar Oferta";
             this.button1.UseVisualStyleBackColor = true;
@@ -64,20 +68,22 @@
             // 
             // ofertarTextBox
             // 
-            this.ofertarTextBox.Location = new System.Drawing.Point(192, 63);
+            this.ofertarTextBox.Location = new System.Drawing.Point(98, 41);
+            this.ofertarTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ofertarTextBox.Name = "ofertarTextBox";
-            this.ofertarTextBox.Size = new System.Drawing.Size(165, 26);
+            this.ofertarTextBox.Size = new System.Drawing.Size(141, 20);
             this.ofertarTextBox.TabIndex = 3;
             // 
             // OfertarPublicacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 161);
+            this.ClientSize = new System.Drawing.Size(253, 115);
             this.Controls.Add(this.ofertarTextBox);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "OfertarPublicacion";
             this.Text = "OfertarPublicacion";
             this.ResumeLayout(false);
